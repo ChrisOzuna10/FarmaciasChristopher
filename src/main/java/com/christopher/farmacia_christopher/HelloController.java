6007d9448d0a4774b73e8fac4224b647a6126e0c
ChrisOzuna10 <233384@ids.upchiapas.edu.mx>
2024-03-18 15:12:37 -0600

    Primera vez

---
+++ src/main/java/com/christopher/farmacia_christopher/HelloController.java
@@ -0,0 +1,14 @@
+package com.christopher.farmacia_christopher;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
