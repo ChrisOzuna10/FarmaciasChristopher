14058c23c655d1f68a8ec1d8c1f92dc6cad50be1
ChrisOzuna10 <233384@ids.upchiapas.edu.mx>
2024-03-18 18:52:17 -0600

    Agregando cosas

---
+++ src/main/java/com/christopher/farmacia_christopher/controladores/CrearVentaControlador.java
@@ -0,0 +1,53 @@
+package com.christopher.farmacia_christopher.controladores;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class CrearVentaControlador {
+
+    @FXML
+    private ListView<?> ListaDeProductos;
+
+    @FXML
+    private ListView<?> ListaDeProductosVendidos;
+
+    @FXML
+    private TextField cantidadDeProducto;
+
+    @FXML
+    private ImageView logo;
+
+    @FXML
+    void AgregarBoton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void ConfirmarVentaBoton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void EliminarBoton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void ModificarBoton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void SalirBoton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void seleccionadoClick(MouseEvent event) {
+
+    }
+
+}
