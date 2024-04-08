1200ac47e410a3a51387945722e1a526f15b0460
ChrisOzuna10 <233384@ids.upchiapas.edu.mx>
2024-04-08 17:56:41 -0600

    Controladores

--- src/main/java/com/christopher/farmacia_christopher/controladores/IniciarSesionController.java
+++ src/main/java/com/christopher/farmacia_christopher/controladores/IniciarSesionController.java
@@ -1,18 +1,21 @@
 package com.christopher.farmacia_christopher.controladores;
 
+import com.christopher.farmacia_christopher.Main;
+import com.christopher.farmacia_christopher.models.Empleado;
 import com.christopher.farmacia_christopher.models.Farmacia;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class IniciarSesionController {
 
-    private Farmacia farmacia;
 
-    public void setFarmacia (Farmacia farmacia){
-        this.farmacia = farmacia;
-    }
 
     @FXML
     private TextField contraseñaTexto;
@@ -23,9 +26,30 @@ public class IniciarSesionController {
     @FXML
     private ImageView logo;
 
-    @FXML
-    void iniciarSesionBoton(MouseEvent event) {
+    private Farmacia farmacia;
 
+    private Stage stage;
+
+    public void setFarmacia (Farmacia farmacia){
+        this.farmacia = farmacia;
+    }
+
+    public void setStage (Stage stage){
+        this.stage = stage;
+    }
+    @FXML
+    void iniciarSesionBoton(MouseEvent event) throws IOException {
+        if (correoTexto.getText().equals("administrador") && contraseñaTexto.getText().equals("contraseña")){
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("InicioSesion.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            stage.setTitle("Inicio de sesion");
+            stage.setScene(scene);
+            stage.show();
+            IniciarSesionController iniciarSesionController = fxmlLoader.getController();
+            iniciarSesionController.setFarmacia(farmacia);
+        } else {
+            //Foreach para buscar a los empleados
+        }
     }
 
 }
